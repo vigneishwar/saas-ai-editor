befd41d41c4fa210f8ed524affadcf192ea079b0
vikki1310 <156143195+vikki1310@users.noreply.github.com>
2026-01-18 20:35:24 +1100

    added auth and userprofile api

--- src/main/java/com/viki/projects/saas_ai_editor/controller/AuthController.java
+++ src/main/java/com/viki/projects/saas_ai_editor/controller/AuthController.java
@@ -1,10 +1,15 @@
 package com.viki.projects.saas_ai_editor.controller;
 
 import com.viki.projects.saas_ai_editor.dto.auth.AuthResponse;
+import com.viki.projects.saas_ai_editor.dto.auth.LoginRequest;
 import com.viki.projects.saas_ai_editor.dto.auth.SignupRequest;
+import com.viki.projects.saas_ai_editor.dto.auth.UserProfileResponse;
 import com.viki.projects.saas_ai_editor.service.AuthService;
+import com.viki.projects.saas_ai_editor.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +18,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final AuthService authService;
+    private AuthService authService;
+    private UserService userService;
 
+    @PostMapping("/signup")
     public ResponseEntity<AuthResponse> signup(SignupRequest request) {
         AuthResponse response = authService.signup(request);
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(LoginRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserProfileResponse>getProfile() {
+        Long userId = 1L; // Placeholder for authenticated user ID
+        return ResponseEntity.ok(userService.getUserProfile(userId));
+    }
+
 }
--- src/main/java/com/viki/projects/saas_ai_editor/service/AuthService.java
+++ src/main/java/com/viki/projects/saas_ai_editor/service/AuthService.java
@@ -1,8 +1,10 @@
 package com.viki.projects.saas_ai_editor.service;
 
 import com.viki.projects.saas_ai_editor.dto.auth.AuthResponse;
+import com.viki.projects.saas_ai_editor.dto.auth.LoginRequest;
 import com.viki.projects.saas_ai_editor.dto.auth.SignupRequest;
 
 public interface AuthService {
     AuthResponse signup(SignupRequest request);
+    AuthResponse login(LoginRequest request);
 }
