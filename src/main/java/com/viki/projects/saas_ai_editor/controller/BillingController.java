ff97c671890e967c5ee8e99241534b0c75c5bc24
vikki1310 <156143195+vikki1310@users.noreply.github.com>
2026-01-22 23:18:31 +1100

    added billing api

--- src/main/java/com/viki/projects/saas_ai_editor/controller/BillingController.java
+++ src/main/java/com/viki/projects/saas_ai_editor/controller/BillingController.java
@@ -2,6 +2,7 @@ package com.viki.projects.saas_ai_editor.controller;
 
 
 import com.viki.projects.saas_ai_editor.dto.subscription.PlanResponse;
+import com.viki.projects.saas_ai_editor.dto.subscription.SubscriptionResponse;
 import com.viki.projects.saas_ai_editor.service.PlanService;
 import com.viki.projects.saas_ai_editor.service.SubscriptionService;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +22,12 @@ public class BillingController {
     @GetMapping("/api/plans")
     public ResponseEntity<List<PlanResponse>> getAvailablePlans() {
         return ResponseEntity.ok(planService.getAllPlans());
+    }
+
+    @GetMapping("/api/me/subscription")
+    public ResponseEntity<SubscriptionResponse> getMySubscription() {
+        Long userId = 1L; // TODO: get user id from auth context
+        return ResponseEntity.ok(subscriptionService.getSubscriptionByUserId(userId));
     }
 }
 
--- src/main/java/com/viki/projects/saas_ai_editor/service/SubscriptionService.java
+++ src/main/java/com/viki/projects/saas_ai_editor/service/SubscriptionService.java
@@ -1,4 +1,7 @@
 package com.viki.projects.saas_ai_editor.service;
 
+import com.viki.projects.saas_ai_editor.dto.subscription.SubscriptionResponse;
+
 public interface SubscriptionService {
+    SubscriptionResponse getSubscriptionByUserId(Long userId);
 }
