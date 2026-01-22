cf58743c670b0691d0c93273bdd7b14c6e04cb7d
vikki1310 <156143195+vikki1310@users.noreply.github.com>
2026-01-23 10:52:15 +1100

    added stripe api

--- src/main/java/com/viki/projects/saas_ai_editor/controller/BillingController.java
+++ src/main/java/com/viki/projects/saas_ai_editor/controller/BillingController.java
@@ -1,13 +1,14 @@
 package com.viki.projects.saas_ai_editor.controller;
 
 
-import com.viki.projects.saas_ai_editor.dto.subscription.PlanResponse;
-import com.viki.projects.saas_ai_editor.dto.subscription.SubscriptionResponse;
+import com.viki.projects.saas_ai_editor.dto.subscription.*;
 import com.viki.projects.saas_ai_editor.service.PlanService;
 import com.viki.projects.saas_ai_editor.service.SubscriptionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,6 +29,19 @@ public class BillingController {
     public ResponseEntity<SubscriptionResponse> getMySubscription() {
         Long userId = 1L; // TODO: get user id from auth context
         return ResponseEntity.ok(subscriptionService.getSubscriptionByUserId(userId));
+    }
+
+    @PostMapping("/api/stripe/checkout")
+    public ResponseEntity<CheckoutResponse>createCheckoutResponse(@RequestBody CheckoutRequest request){
+        Long userId = 1L; // TODO: get user id from auth context
+        CheckoutResponse response = subscriptionService.createCheckoutSessionUrl(request, userId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/api/stripe/portal")
+    public ResponseEntity<StripePortalResponse> openCustomerPortal(){
+        Long userId = 1L;
+        return ResponseEntity.ok(subscriptionService.openCustomerPortal(userId));
     }
 }
 
---
+++ src/main/java/com/viki/projects/saas_ai_editor/dto/subscription/CheckoutResponse.java
@@ -0,0 +1,4 @@
+package com.viki.projects.saas_ai_editor.dto.subscription;
+
+public record CheckoutResponse(String checkoutUrl) {
+}
